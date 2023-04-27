4ebc8a8ac4dcee150090b275207167835a8f6a30
GalacticLemonade <aidenmoore1820@gmail.com>
2023-04-26 18:36:00 -0600

    e

--- forge.config.js
+++ forge.config.js
@@ -7,4 +7,17 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'GalacticLemonade',
+          name: 'offlinegames'
+        },
+        authToken: "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",
+        prerelease: true
+      }
+    }
+  ]
 };
