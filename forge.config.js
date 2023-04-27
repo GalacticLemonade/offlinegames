806fc2838f2b0134cb6d6536148e37b94a711207
GalacticLemonade <aidenmoore1820@gmail.com>
2023-04-26 19:28:30 -0600

    update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,21 +1,36 @@
+require('dotenv').config()
+
 module.exports = {
   packagerConfig: { 
-    
-   },
+    appCopyright: 'Tiny Borb, Inc.'
+  },
   rebuildConfig: { 
 
-   },
+  },
   makers: [ 
-
-   ],
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    }
+  ],
   publishers: [ 
-
-   ],
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'GalacticLemonade',
+          name: 'offlinegames'
+        },
+        prerelease: false,
+        authToken: ""
+      }
+    }
+  ],
   plugins: [ 
 
-   ],
+  ],
   hooks: { 
 
-   },
+  },
   buildIdentifier: 'v1.0.2'
 }
