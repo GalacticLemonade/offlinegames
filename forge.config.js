ee5494d9f03982117811620e283ad5b2be9c515c
GalacticLemonade <aidenmoore1820@gmail.com>
2023-04-26 19:18:53 -0600

    update forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  
-};
+  packagerConfig: { 
+    
+   },
+  rebuildConfig: { 
+
+   },
+  makers: [ 
+
+   ],
+  publishers: [ 
+
+   ],
+  plugins: [ 
+
+   ],
+  hooks: { 
+
+   },
+  buildIdentifier: 'v1.0.2'
+}
