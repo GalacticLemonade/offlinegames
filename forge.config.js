b0912577442ecc7ee22b2162408c0464e73cd7f1
GalacticLemonade <aidenmoore1820@gmail.com>
2023-04-26 19:17:28 -0600

    update forge.config.js and package.json

--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,3 @@
 module.exports = {
-    packagerConfig: {},
-    rebuildConfig: {},
-    makers: [
-      {
-        name: '@electron-forge/maker-squirrel',
-        config: {},
-      },
-    ],
-    publishers: [
-      {
-        name: '@electron-forge/publisher-github',
-        config: {
-          repository: {
-            owner: 'GalacticLemonade',
-            name: 'offlinegames'
-          },
-          authToken: "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",
-          prerelease: true
-        }
-      }
-    ]
-  };
   
+};
